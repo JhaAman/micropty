151a59489d23be2f714e5a7bc4ba928208faaace
Aman Jha <gunujha@gmail.com>
2021-09-13 14:36:54 -0400

    frameless window, black background

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,6 +1,8 @@
 var os = require("os");
 var pty = require("node-pty");
 var Terminal = require("xterm").Terminal;
+// var FitAddon = require("xterm-addon-fit");
+// import { FitAddon } from "xterm-addon-fit";
 
 // Initialize node-pty with an appropriate shell
 const shell = process.env[os.platform() === "win32" ? "COMSPEC" : "SHELL"];
@@ -14,7 +16,11 @@ const ptyProcess = pty.spawn(shell, [], {
 
 // Initialize xterm.js and attach it to the DOM
 const xterm = new Terminal();
+// const fitAddon = new FitAddon();
+// xterm.loadAddon(fitAddon);
 xterm.open(document.getElementById("xterm"));
+// Make the terminal's size and geometry fit the size of #xterm
+// fitAddon.fit();
 
 // Setup communication between xterm.js and node-pty
 xterm.onData((data) => ptyProcess.write(data));
